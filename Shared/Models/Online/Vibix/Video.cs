10ffd1d1e0eeeaf162317a33ab0a8bfd2d212135
Vitaly <36040586+bennington111@users.noreply.github.com>
2026-02-18 15:33:59 +0200

    Remove properties from Video and Voiceover classes
    Removed properties from Video class and Voiceover class.

--- Shared/Models/Online/Vibix/Video.cs
+++ Shared/Models/Online/Vibix/Video.cs
@@ -1,22 +1,9 @@
-namespace Shared.Models.Online.Vibix
+﻿namespace Shared.Models.Online.Vibix
 {
     public class Video
     {
         public string iframe_url { get; set; }
 
         public string type { get; set; }
-		
-		public string embed_code { get; set; }
-		
-		public long id { get; set; }
-		
-		public Voiceover[] voiceovers { get; set; }
-    }
-
-    public class Voiceover
-    {
-        public int id { get; set; }
-
-        public string name { get; set; }
     }
 }
